8d7b35a3b57132fe0b442955dc29fe915921b136
danielaj2004 <danielajoya12@gmail.com>
2025-02-16 21:45:50 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -5,3 +5,11 @@ function calculateInvoice(subtotal, taxRate, discount) {
 }; // function to calculate invoice total
 console.log(calculateInvoice(100, 0.08, 5)); // Total Invoice: $103.00
 console.log(calculateInvoice(500, 0.1, 20)); // Total Invoice: $530.00
+
+// Task 2
+function calculateHourlyWage(salary, hoursPerWeek) {
+    const hourlyWage = salary / (hoursPerWeek * 52);
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`;
+}
+console.log(calculateHourlyWage(52000, 40)); // Hourly Wage: $25.00
+console.log(calculateHourlyWage(75000, 35)); // Hourly Wage: $41.21
