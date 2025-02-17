83d10a350686398a8721cc79a4fbdc9f19383493
danielaj2004 <danielajoya12@gmail.com>
2025-02-16 21:53:35 -0500

    Task 3 - Customer Loyalty Discount.

--- cc_7.js
+++ cc_7.js
@@ -13,3 +13,21 @@ function calculateHourlyWage(salary, hoursPerWeek) {
 }
 console.log(calculateHourlyWage(52000, 40)); // Hourly Wage: $25.00
 console.log(calculateHourlyWage(75000, 35)); // Hourly Wage: $41.21
+
+// Task 3
+let calculateLoyaltyDiscount = (amount, years) => {
+    let discountRate
+    if (years >= 5) {
+        discountRate = 0.15;
+    }
+    else if (years >= 3) {
+        discountRate = 0.10;
+    }
+    else if (years < 3) {
+        discountRate = 0.05;
+    }
+    discountedPrice = amount - (amount * discountRate);
+    return `Discounted Price: $${discountedPrice.toFixed(2)}`;
+}
+console.log(calculateLoyaltyDiscount(100, 6)); // Discounted Price: $85.00
+console.log(calculateLoyaltyDiscount(200, 2)); // Discounted Price: $190.00
