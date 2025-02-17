1a84e4263a3e73238b25101c3be099d09b5461ad
danielaj2004 <danielajoya12@gmail.com>
2025-02-16 22:18:14 -0500

    Task 6 - Filtering High-Value Transactions

--- cc_7.js
+++ cc_7.js
@@ -56,3 +56,10 @@ function calculateLoanInterest(principal, rate, years) {
 }
 console.log(calculateLoanInterest(1000, 0.05, 3)); // Total Interest: $150.00
 console.log(calculateLoanInterest(5000, 0.07, 5)); // Total Interest: $1750.00
+
+// Task 6
+let transactions = [500, 1200, 3000, 800, 2200]; // array fo transactions
+function filterHighValueTransactions(transactions, filterFunction) {
+    return transactions.filter(filterFunction);
+}
+console.log(filterHighValueTransactions(transactions, amount => amount > 1000)); // [1200, 3000, 2200]
